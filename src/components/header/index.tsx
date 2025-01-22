f53114dbba8c454718b38700dda292d9ccd06d7a
YanGraim <yangraim@gmail.com>
2025-01-22 15:17:39 -0300

    estrutura e estiliza component Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,14 @@
+import styles from './header.module.css';
+import logoImg from './../../assets/logo.svg'
+import { Link } from 'react-router-dom';
+
 
 export function Header() {
     return (
-        <header>
-            <h1>Teste Header</h1>
+        <header className={styles.container}>
+            <Link to={"/"}>
+                <img src={logoImg} alt="logo Cripto App" />
+            </Link>
         </header>
     )
 }
