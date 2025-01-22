2423464b94b9329f7933d3b47c5982682744046f
YanGraim <yangraim@gmail.com>
2025-01-22 14:46:23 -0300

    cria pages

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,7 @@
+export function Detail() {
+    return (
+        <div>
+            <h1>Pagina detalhe da moeda</h1>
+        </div>
+    )
+}
---
+++ src/pages/notFound/index.tsx
@@ -0,0 +1,7 @@
+export function NotFound() {
+    return (
+        <div>
+            <h1>Pagina 404, essa página não existe!</h1>
+        </div>
+    )
+}
