dc411cdb709a71bb66a67c87423140c7b9b8a700
YanGraim <yangraim@gmail.com>
2025-01-23 11:36:23 -0300

    utiliza useState para receber info do input e useNavigate para redirecionar rota de acordo com o que o usuario escreveu no input

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,29 @@
+import { useState, FormEvent} from 'react'
 import styles from './home.module.css'
 import { BsSearch } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export function Home() {
+    const [input, setInput] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+
+        if(input === "") return;
+        navigate(`/detail/${input}`)
+    }
+
+
     return (
         <main className={styles.container}>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <input 
                  type="text"
                  placeholder='Digite o nome da moeda...'
+                 value={input}
+                 onChange={(e) => setInput(e.target.value)}
                 />
 
                 <button type='submit'>
@@ -55,6 +70,10 @@ export function Home() {
                     </tr>
                 </tbody>
             </table>
+
+            <button className={styles.buttonMore}>
+                Carregar mais
+            </button>
         </main>
     )
 }
