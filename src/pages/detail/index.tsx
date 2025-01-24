cddc30ced5236ffbab091e766ee1a039d2f5cc0d
YanGraim <yangraim@gmail.com>
2025-01-24 11:03:29 -0300

    feat: consumindo a api em detail, utilizando o parametro cripto

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,8 +1,25 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 
 export function Detail() {
     const { cripto } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function getCoin() {
+            try {
+                fetch(`https://api.coincap.io/v2/assets${cripto}`)
+                .then(response => response.json())
+                .then((data) => {
+                    console.log(data);
+                })
+            } catch (error) {
+                console.log(error);
+                navigate("/");
+            }
+        }
+        getCoin();
+    }, [cripto, navigate])
 
     return (
         <div>
