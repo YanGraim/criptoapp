be9aabeb3afe2bf7c2a5d3cfc65817c1a4b8e75a
YanGraim <yangraim@gmail.com>
2025-01-24 12:11:03 -0300

    feat: importa propriedades, formata valores

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,17 +1,54 @@
-import { useEffect, useState } from "react"
-import { useParams, useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { CoinProps } from "../home";
+
+
+interface ResponseData {
+    data: CoinProps;
+}
+
+interface ErrorData {
+    error: string;
+}
+
+type DataProps = ResponseData | ErrorData
+
 
 export function Detail() {
     const { cripto } = useParams();
     const navigate = useNavigate();
+    const [coin, setCoin] = useState<CoinProps>()
 
     useEffect(() => {
         async function getCoin() {
             try {
-                fetch(`https://api.coincap.io/v2/assets${cripto}`)
+                fetch(`https://api.coincap.io/v2/assets/${cripto}`)
                 .then(response => response.json())
-                .then((data) => {
-                    console.log(data);
+                .then((data: DataProps) => {
+                    if("error" in data) {
+                        navigate("/");
+                        return;
+                    }
+                    
+                    const price = Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "USD"
+                    })
+        
+                    const priceCompact = Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                        notation: "compact"
+                    })
+
+                    const resultData = {
+                        ...data.data,
+                        formatedPrice: price.format(Number(data.data.priceUsd)),
+                        formatedMarket: priceCompact.format(Number(data.data.marketCapUsd)),
+                        formatedVolume: priceCompact.format(Number(data.data.volumeUsd24Hr))
+                    }
+
+                    setCoin(resultData);
                 })
             } catch (error) {
                 console.log(error);
