03500d7f86a98d5a92cc0821d24d47a259373e97
YanGraim <yangraim@gmail.com>
2025-01-24 10:55:07 -0300

    feat: importa useParams dentro de details

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,7 +1,12 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 export function Detail() {
+    const { cripto } = useParams();
+
     return (
         <div>
-            <h1>Pagina detalhe da moeda</h1>
+            <h1>Pagina detalhe da moeda {cripto}</h1>
         </div>
     )
 }
